58c9a1a3e2432f6bb71b0a487d7d324dc46a0746
Ben Matthew Pinto <benmattpinto@gmail.com>
2018-02-24 18:20:24 -0500

    timer stuff
    lmao

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+	public static float m_timer = 60.0f;
+	public Text m_text;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		m_timer -= Time.deltaTime;
+		m_text.text = Mathf.FloorToInt(m_timer).ToString();
+		
+	}
+}
