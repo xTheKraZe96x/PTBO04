3fb319fd907c2c13365e63ffe76f79799a9bc5b4
Shawn Baldwin <TheKraZe@Rogers.com>
2018-02-24 10:10:10 -0500

    Simple start screen with SP or MP options.

---
+++ Assets/Scripts/LoadMultiPlayer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadMultiPlayer : MonoBehaviour {
+
+	public void RunMP(){
+		Debug.Log("Load Multiplayer");
+	}
+}
---
+++ Assets/Scripts/LoadSinglePlayer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadSinglePlayer : MonoBehaviour {
+
+	public void RunSP(){
+		Debug.Log("Load Single Player");
+	}
+}
